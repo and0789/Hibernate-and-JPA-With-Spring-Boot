47f3ce34c94ccf33dd84ba7d5aad2a22fa22080b
andreseptian <andreseptian07@icloud.com>
2022-12-22 13:14:18 +0700

    Build Simple API

---
+++ src/main/java/com/itc/jpacruddemo/CourseController.java
@@ -0,0 +1,20 @@
+package com.itc.jpacruddemo;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+public class CourseController {
+
+    @RequestMapping("/courses")
+    public List<Course> retrieveAllCourses() {
+        return Arrays.asList(
+                new Course (1L, "Learn AWS", "Institut Teknologi Cirebon"),
+                new Course (2L, "Learn DevOps", "Institut Teknologi Cirebon"),
+                new Course (3L, "Learn Software Engineer", "Institut Teknologi Cirebon")
+        );
+    }
+}
