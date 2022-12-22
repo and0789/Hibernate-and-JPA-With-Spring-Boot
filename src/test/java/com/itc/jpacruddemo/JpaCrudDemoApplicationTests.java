278e2ca1edc9c2fd7ae1cd3035c518a47a89f920
andreseptian <andreseptian07@icloud.com>
2022-12-22 12:45:37 +0700

    Project Started

---
+++ src/test/java/com/itc/jpacruddemo/JpaCrudDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itc.jpacruddemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaCrudDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
